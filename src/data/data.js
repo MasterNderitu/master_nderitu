ed966cf4b27a82f8a230da48bc4059911402636f
Nderitu <masternderitu@gmail.com>
2024-03-13 03:09:23 +0300

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+import Magz from '../assets/projects/magomano.png'
+import Guru from '../assets/projects/nderitus.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Magz,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Guru,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Magz,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:Guru,
+        github:"",
+        live:"",
+    },
+    {
+        id:5,
+        name:"React JS Application",
+        image:Magz,
+        github:"",
+        live:"",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:Guru,
+        github:"",
+        live:"",
+    },
+
+
+]
